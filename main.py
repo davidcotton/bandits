a6b73f28498a80670cbd4fd3c2b154952d17f5b7
David Cotton <davidcotton@users.noreply.github.com>
2023-08-09 11:39:47 +1000

    feat: remove old code

--- main.py
+++ main.py
@@ -3,41 +3,12 @@ from datetime import datetime
 
 import mlflow
 
-from bandits.bandits import build_bandits, EpsilonGreedySampler
-from bandits.bandits.exp3_bandit import Exp3Bandit
+from bandits.bandits import build_bandits
 from bandits.envs import build_env
 from bandits.measures import build_measures
 from bandits.runner import Runner
 from bandits.utils import get_summaries_dir, load_config
 
-POLICIES = {
-    # 'random': (RandomSampler, {}),
-    'epsilon greedy (10%)': (EpsilonGreedySampler, {'epsilon': 0.1}),
-    # # 'epsilon greedy (5%)': (EpsilonGreedySampler, {'epsilon': 0.05}),
-    'epsilon greedy (1%)': (EpsilonGreedySampler, {'epsilon': 0.01}),
-    # # 'epsilon weighted (alpha 0.1)': (EpsilonGreedyWeightedSampler, {'epsilon': 0.1, 'alpha': 0.1}),
-    # # 'epsilon weighted (alpha 0.01)': (EpsilonGreedyWeightedSampler, {'epsilon': 0.1, 'alpha': 0.01}),
-    # # 'softmax (0.01)': (SoftmaxSampler, {'temperature': 0.01}),
-    # 'softmax (0.1)': (SoftmaxSampler, {'temperature': 0.1}),
-    # # 'softmax (0.2)': (SoftmaxSampler, {'temperature': 0.2}),
-    # # 'softmax (0.3)': (SoftmaxSampler, {'temperature': 0.3}),
-    # 'softmax (0.4)': (SoftmaxSampler, {'temperature': 0.4}),
-    # # 'softmax (0.5)': (SoftmaxSampler, {'temperature': 0.5}),
-    # # 'softmax (0.6)': (SoftmaxSampler, {'temperature': 0.6}),
-    # # 'softmax (0.7)': (SoftmaxSampler, {'temperature': 0.7}),
-    # # 'softmax (0.8)': (SoftmaxSampler, {'temperature': 0.8}),
-    # # 'softmax (0.9)': (SoftmaxSampler, {'temperature': 0.9}),
-    # 'ucb1': (UCB1Sampler, {}),
-    # 'exp3': (Exp3Bandit, {}),
-    # 'exp3 (gamma=0.01)': (Exp3Bandit, {'gamma': 0.01}),
-    'exp3 (gamma=0.05)': (Exp3Bandit, {'gamma': 0.05}),
-    'exp3 (gamma=0.07)': (Exp3Bandit, {'gamma': 0.07}),
-    'exp3 (gamma=0.1)': (Exp3Bandit, {'gamma': 0.1}),
-    'exp3 (gamma=0.2)': (Exp3Bandit, {'gamma': 0.2}),
-    'exp3 (gamma=0.3)': (Exp3Bandit, {'gamma': 0.3}),
-    # 'exp3 (gamma=0.4)': (Exp3Bandit, {'gamma': 0.4}),
-    # 'exp3 (gamma=0.5)': (Exp3Bandit, {'gamma': 0.5}),
-}
 DEFAULT_MLFLOW_SERVER_URI = "http://127.0.0.1:5000"
 
 
